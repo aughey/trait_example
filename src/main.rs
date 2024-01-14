f09d50d9e1f92ca221c2e6c0900f748088ebc871
John Aughey <jha@aughey.com>
2024-01-14 11:43:50 -0600

    playground

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,46 @@
-use trait_example::InserttableContainer;
-
-fn do_work_to_vec(container: &mut Vec<i32>) {
-    container.push(42);
+pub trait InserttableContainer<Item> {
+    fn insert(&mut self, item: Item);
 }
 
-fn do_work_to_generic<Container>(container: &mut Container)
+impl<T> InserttableContainer<T> for Vec<T> {
+    fn insert(&mut self, item: T) {
+        self.push(item);
+    }
+}
+
+fn do_work_to_vec(container: &mut Vec<i32>, val: i32) {
+    container.push(val);
+}
+
+fn do_work_to_generic<Container>(container: &mut Container, val: i32)
 where
     Container: InserttableContainer<i32>,
 {
-    container.insert(42);
+    container.insert(val);
 }
 
 
 fn main() {
     let mut v = vec![];
-    do_work_to_vec(&mut v);
+    do_work_to_vec(&mut v, 40_000_000);
     println!("v: {:?}", v);
 
-    do_work_to_generic(&mut v);
+    let mut v = vec![];
+    do_work_to_vec(&mut v, 50_000_000);
     println!("v: {:?}", v);
+
+    let mut v = vec![];
+    do_work_to_generic(&mut v, 60_000_000);
+    println!("v: {:?}", v);
+    
+        let mut v = vec![];
+    do_work_to_generic(&mut v, 70_000_000);
+    println!("v: {:?}", v);
+
+}
+
+#[cfg(target_arch = "wasm32")]
+#[no_mangle]
+pub extern "C" fn callmain() {
+    main()
 }
